7f3df27195e0cb8697f99fc664b971b8a6ebd110
Alexey Zhokhov <donbeave@gmail.com>
2014-12-24 16:55:28 +0800

    Upgrade for Grails plugins.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,4 @@
-
-if(System.getenv('TRAVIS_BRANCH')) {
+if (System.getenv('TRAVIS_BRANCH')) {
     grails.project.repos.grailsCentral.username = System.getenv("GRAILS_CENTRAL_USERNAME")
     grails.project.repos.grailsCentral.password = System.getenv("GRAILS_CENTRAL_PASSWORD")
 }
@@ -12,8 +11,8 @@ grails.project.source.level = 1.6
 grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
 
-    inherits "global"
-    log "warn"
+    inherits 'global'
+    log 'warn'
 
     repositories {
         mavenLocal()
@@ -26,15 +25,15 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        compile ':spring-security-core:2.0-RC3'
-        compile ':oauth:2.5'
-        compile ':codenarc:0.21'
+        compile ':spring-security-core:2.0-RC4'
+        compile ':oauth:2.6.1'
+        compile ':codenarc:0.22'
         // we need this to compile in Grails 2.4 RC...
-        compile ':hibernate:3.6.10.14', {
+        compile ':hibernate:3.6.10.18', {
             export = false
         }
 
-        build ':release:3.0.1', ':rest-client-builder:2.0.1', ':testapps:0.5.0', {
+        build ':release:3.0.1', ':rest-client-builder:2.0.3', ':testapps:0.5.0', {
             export = false
         }
     }
@@ -51,7 +50,7 @@ codenarc {
             title = 'Spring Security OAuth plugin CodeNarc Report'
         }
     }
-    ruleSetFiles='file:grails-app/conf/CodeNarcRuleSet.groovy'
+    ruleSetFiles = 'file:grails-app/conf/CodeNarcRuleSet.groovy'
     maxPriority1Violations = 0
     maxPriority2Violations = 0
     maxPriority3Violations = 0
